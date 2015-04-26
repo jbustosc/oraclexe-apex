cf8bd49fe651ac0c273ba7c232d8f5831220b774
Trent Schafer <trent.schafer@gmail.com>
2015-04-27 09:11:26 +1000

    Install ratom from submodule rmate

--- scripts/ratom.sh
+++ scripts/ratom.sh
@@ -3,7 +3,4 @@
 #*** REMOTE ATOM ***
 #https://github.com/randy3k/remote-atom
 #This is to make files easier to edit in Atom
-cd $OOS_SOURCE_DIR/tmp
-${OOS_UTILS_DIR}/download.sh https://raw.githubusercontent.com/aurora/rmate/master/rmate
-chmod +x rmate
-mv rmate /usr/local/bin/ratom
+cp ${OOS_SOURCE_DIR}/extras/rmate/rmate /usr/local/bin/ratom
